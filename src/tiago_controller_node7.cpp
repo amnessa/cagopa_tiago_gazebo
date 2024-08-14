04baf48a227ce5831f400aea9f9269c6866a1783
cagolin2 <cagdas96vkp@gmail.com>
2024-08-14 20:35:34 +0300

    testing values

--- src/tiago_controller_node7.cpp
+++ src/tiago_controller_node7.cpp
@@ -8,18 +8,19 @@ using FollowJointTrajectory = control_msgs::action::FollowJointTrajectory;
 using GoalHandleFollowJointTrajectory = rclcpp_action::ClientGoalHandle<FollowJointTrajectory>;
 
 // ros2 launch tiago_gazebo tiago_gazebo.launch.py is_public_sim:=True [arm_type:=no-arm]
+// need to find a way to feed different motions to the robot (maybe a motions library)
 class TrajectoryActionClient : public rclcpp::Node
 {
 public:
 
     TrajectoryActionClient() : Node("trajectory_action_client")
     {
-        move7={};
-        move2={};
-        move3={};
-        move4={};
-        move5={};
-        move6={};
+        x_move={};
+        z_move={};
+        x_move2={};
+        y_move={};
+        y_move2={};
+        z_move2={};
         cago_initial={};
         RCLCPP_INFO(this->get_logger(), "IN Contstructor");
         this->tiago_control_client_ = rclcpp_action::create_client<FollowJointTrajectory>(
@@ -47,23 +48,17 @@ private:
         std::vector<trajectory_msgs::msg::JointTrajectoryPoint> points;
 
         // An initial Position for the robot tahta min mak H -> robot min 78 cm tahta min 90cm max 207cm
-        cago_initial={1.565, 0.939, -0.056, 0.975, 1.738, 0.023, -0.034};  //{-1.1470 0.3418 -2.1480}
-        // cago_reference1={2.679, 0.053, 1.501, 1.158, 0.000, 0.000, 0.000};  {-3.224,-3.47,0.4543}
-        // cago_reference2={0.253, 0.053, -1.666, 1.327, 0.000, 0.000, 0.000}; {-0.6227,-1.749,0.8144}
-        // cago_reference3={0.253, 1.021, -1.371, 1.539, 0.000, 0.000, 0.000};  {-1.742,-1.611,-0.9923}
-        move2={1.5532  ,  1.0298  , -0.2537  ,  1.1005  ,  1.8147   , 0.2033,-0.034}; //-0.6122,0.2041,-2.188
-        //move3={1.3439  ,  0.9616  , -0.3567   , 0.9943  ,  1.6818  ,  0.2681, -0.034}; //-0.6122,0.6122,-2.046
-        move4={1.3137  ,  0.7331  , -0.0235   , 0.7603  ,  1.4722  , -0.0147,-0.034}; //-1.429,1.02,-1.915
-        move5={1.6670  ,  1.1319   , 0.3988    ,1.1053 ,   1.6375  , -0.3858,-0.034};//-2.245,0.2041,-2.212
-        //move6={1.9253  ,  1.1397  ,  0.2383   , 1.0698  ,  1.9624  , -0.2498, -0.034}; //-1.429,-0.6122,-2.485
-        move7={1.5532  ,  1.0298  , -0.2537  ,  1.1005   ,1.8147  ,  0.2033,-0.034}; //-0.6122,0.2041,-2.188
-
+        cago_initial={1.565, 0.939, -0.056, 0.975, 1.738, 0.023, -0.034};
+        x_move={-0.0608  ,  1.0785  , -0.3617  ,  0.9814  , -1.7359   ,-0.2863  ,  1.9721}; //plus 1
+        z_move={1.7933  ,  1.8351  , -0.4234 ,   1.6007  ,  0.9507  ,  0.4831  ,  0.8766}; //negative 1
+        x_move2={-0.0608  ,  1.0785  , -0.3617  ,  0.9814  , -1.7359  , -0.2863   , 1.9721}; //negative 1
+        y_move={-0.8264 ,   1.0822 ,  -0.3576  ,  0.8897 ,  -1.9505 ,  -0.3266  ,  1.4360};//plus 1
+        y_move2={0.2854  ,  1.2198  , -0.4036 ,   0.9391   ,-2.1732  , -0.4148  ,  2.7563}; //negative 0.5
+        z_move2={1.7933   , 1.8351  , -0.4234 ,   1.6007  ,  0.9507   , 0.4831 ,   0.8766}; //negative 1
+        //                  1    2    3    4    5    6    7
         // % Set joint limits for the first joint
-
         // robot.links(1).qlim = [-pi/2, pi/2];
-
         // trajectory_msgs::msg::JointTrajectoryPoint::time_from_start
-
         // arm_1_joint: <limit effort="43.0" lower="0.0" upper="2.748893571891069" velocity="1.95"/>
         // arm_2_joint: <limit effort="43.0" lower="-1.5707963267948966" upper="1.090830782496456" velocity="1.95"/>
         // arm_3_joint: <limit effort="26" lower="-3.5342917352885173" upper="1.5707963267948966" velocity="2.35"/>
@@ -73,45 +68,39 @@ private:
         // arm_7_joint: <limit effort="6.6" lower="-2.0943951023931953" upper="2.0943951023931953" velocity="1.76"/>
         // arm_tool_joint: fixed
 
-        //T6 = [0.9897   0.0234   0.1409  -1.1470; -0.1401 -0.0323  0.9896  0.3418;  0.0277 -0.9992  -0.0287  -3.1480;   0    0  0  1.0000]
-        // T6 =[      
-        //     0.9897    0.0234    0.1409   -1.1470;
-        //    -0.1401   -0.0323    0.9896    0.3418;
-        //     0.0277   -0.9992   -0.0287   -3.1480;
-        //          0         0         0    1.0000]
-        //ros2 action send_goal /play_motion2 play_motion2_msgs/action/PlayMotion2 "{motion_name: home, skip_planning: false}"
+        // T7 =[0.9940 -0.1072 0.0234 -1.147; -0.1064 -0.9938 -0.0323 0.3418; 0.0267 0.0296 -0.9992 -2.148; 0 0 0 1]
+        //ros2 action send_goal /play_motion2 play_motion2_msgs/action/PlayMotion2 "{motion_name: tuck, skip_planning: false}"
         trajectory_msgs::msg::JointTrajectoryPoint p1,p2,p3,p4,p5,p6,p7;
         
         p1.positions = cago_initial;
         p1.time_from_start = rclcpp::Duration::from_seconds(2.0);
         points.push_back(p1);
 
-        p2.positions= move2;
-        p2.time_from_start = rclcpp::Duration::from_seconds(4.0);
-        points.push_back(p2);
+        // p2.positions= x_move;
+        // p2.time_from_start = rclcpp::Duration::from_seconds(6.0);
+        // points.push_back(p2);
 
-        // p3.positions=move3;
-        // p3.time_from_start=rclcpp::Duration::from_seconds(6.0);
-        // points.push_back(p3);
-
-        p4.positions=move4;
-        p4.time_from_start=rclcpp::Duration::from_seconds(6.0);
+        p4.positions=x_move2;
+        p4.time_from_start=rclcpp::Duration::from_seconds(4.0);
         points.push_back(p4);
 
-        p5.positions=move5;
-        p5.time_from_start=rclcpp::Duration::from_seconds(8.0);
+        p5.positions=y_move;
+        p5.time_from_start=rclcpp::Duration::from_seconds(6.0);
         points.push_back(p5);
 
-        // p6.positions=move6;
-        // p6.time_from_start = rclcpp::Duration::from_seconds(10.0);
-        // points.push_back(p6);
+        p6.positions=y_move2;
+        p6.time_from_start=rclcpp::Duration::from_seconds(8.0);
+        points.push_back(p6);
 
-        p7.positions=move7;
-        p7.time_from_start = rclcpp::Duration::from_seconds(10.0);
-        points.push_back(p7);
+        p3.positions=z_move;
+        p3.time_from_start = rclcpp::Duration::from_seconds(10.0);
+        points.push_back(p3);
+
+        p7.positions=z_move2;
+        p7.time_from_start = rclcpp::Duration::from_seconds(12.0);
 
         p1.positions = cago_initial;
-        p1.time_from_start = rclcpp::Duration::from_seconds(12.0);
+        p1.time_from_start = rclcpp::Duration::from_seconds(14.0);
         points.push_back(p1);
 
         //point.velocities = 
@@ -175,8 +164,9 @@ private:
     }
     
     std::vector<double> cago_initial;
-    std::vector<double> move2,move3,move4,move5,move6,move7;
-    
+    std::vector<double> x_move,x_move2;
+    std::vector<double> z_move,z_move2;
+    std::vector<double> y_move,y_move2;
 
     rclcpp_action::Client<FollowJointTrajectory>::SharedPtr tiago_control_client_;
     GoalHandleFollowJointTrajectory::SharedPtr goal_handle_;
