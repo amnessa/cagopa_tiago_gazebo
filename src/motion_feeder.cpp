b4189163859d65688443e7e252b3a5693357d2ce
cagopa <cagdas96vkp@gmail.com>
2024-08-22 16:41:43 +0300

    next level added

---
+++ src/motion_feeder.cpp
@@ -0,0 +1,20 @@
+
+//there should be motion list that can be called with single letter
+
+
+//We need to feed motions through console
+
+// can be completed through lifecycle node -in configuration takes the console string as argument
+
+//then we switch to action move which then starts to send goals
+
+// string array -> loop through motions(shall be generated from easier method line moveit2)
+
+//near future -> migrate kinematic calculations here
+
+//for future = put empty spaces after motions, create a string object from motions
+//-> then fill the plane left from right
+
+//also for future= create plane function on own with 3 point input from arm end effector
+
+
