1112c16aa657ddcc2b5e2c12b9c0c1be25544e08
cagopa <cagdas96vkp@gmail.com>
2025-08-06 11:26:42 +0000

    added velocity to yaml

--- ur_config/launch/moveit_rviz.launch.py
+++ ur_config/launch/moveit_rviz.launch.py
@@ -1,7 +1,14 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_moveit_rviz_launch
+import os
+from ament_index_python.packages import get_package_share_directory
+import xacro
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur10e", package_name="ur_config").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("ur10e", package_name="ur_config")
+        .robot_description(file_path="config/ur10e.urdf.xacro")
+        .to_moveit_configs()
+    )
     return generate_moveit_rviz_launch(moveit_config)
