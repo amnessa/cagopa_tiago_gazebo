0b6602bcbe4cb9deca1070e3c9bfd5094be55a42
cagopa <cagdas96vkp@gmail.com>
2025-08-06 11:32:55 +0000

    Update UR10 launch file to use processed xacro for robot description

--- ur_config/launch/moveit_rviz.launch.py
+++ ur_config/launch/moveit_rviz.launch.py
@@ -6,9 +6,20 @@ import xacro
 
 
 def generate_launch_description():
+    # Get the path to the xacro file from the ur_config package
+    xacro_file = os.path.join(
+        get_package_share_directory("ur_config"), "config", "ur10e.urdf.xacro"
+    )
+
+    # Process the xacro file to generate the robot description XML
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {"robot_description": robot_description_config.toxml()}
+
+    # Build the MoveIt configuration, passing the pre-processed robot description
     moveit_config = (
         MoveItConfigsBuilder("ur10e", package_name="ur_config")
-        .robot_description(file_path="config/ur10e.urdf.xacro")
+        .robot_description(mappings=robot_description)
         .to_moveit_configs()
     )
+
     return generate_moveit_rviz_launch(moveit_config)
